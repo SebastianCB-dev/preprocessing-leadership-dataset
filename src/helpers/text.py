775271143cfd170409d3c99713a9d9c20e9edabc
SebastianCB <sebastian.carrillob01@gmail.com>
2024-06-05 20:47:11 -0500

    Feat: add helpers to delete blank spaces, accents and unknown words

--- src/helpers/text.py
+++ src/helpers/text.py
@@ -1,4 +1,49 @@
-# helpers
+
 def delete_accented_chars(text):
-    text.replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u')
+    """
+    Removes accented characters from the given text and converts it to lowercase.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        str: The text with accented characters removed and converted to lowercase.
+    """
+    text = text.lower()
+    text = text.replace('á', 'a')
+    text = text.replace('é', 'e')
+    text = text.replace('í', 'i')
+    text = text.replace('ó', 'o')
+    text = text.replace('ú', 'u')
     return text
+
+def delete_spanish_letters(text):
+    """
+    Deletes Spanish letters from the given text.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        str: The text with Spanish letters removed.
+    """
+    text = text.lower()
+    text.replace('ñ', 'n')
+    return text
+
+def delete_spaces(text):
+    """
+    Deletes spaces from the given text.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        str: The text with spaces removed.
+    """
+    text_split = text.split(' ')
+    # filter removes empty strings
+    text_split = list(filter(None, text_split))
+    text_split = list(filter(lambda x: x != ' ', text_split))
+
+    return ' '.join(text_split)
