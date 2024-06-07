5ab9137bd7fb3f40cf77f40efdf080d87d6f5caf
SebastianCB <sebastian.carrillob01@gmail.com>
2024-06-06 19:13:21 -0500

    Refactor preprocessing method to include deletion of non-vocabulary words

--- src/helpers/text.py
+++ src/helpers/text.py
@@ -1,3 +1,4 @@
+import re
 
 def delete_accented_chars(text):
     """
@@ -47,3 +48,18 @@ def delete_spaces(text):
     text_split = list(filter(lambda x: x != ' ', text_split))
 
     return ' '.join(text_split)
+
+def delete_not_vocabulary_words(text: str) -> str:
+    """
+    Deletes non-vocabulary words from the given text.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        str: The text with non-vocabulary words removed.
+    """
+    text = text.lower()
+    regex = re.compile(r'[^a-z\s]')
+    text = re.sub(regex, '', text)
+    return text
